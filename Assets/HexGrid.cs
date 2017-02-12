98f5344708e025593f0191fc231c10c52e17981a
max <max.pervukhin@gmail.com>
2017-02-13 02:37:11 +0700

    upd

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -23,12 +23,12 @@ public class HexGrid : MonoBehaviour {
     const float radius = 2.0f;
 	// Use this for initialization
 	void Start () {
-        
+       Generate(); 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Generate();
+        
     }
 
 
